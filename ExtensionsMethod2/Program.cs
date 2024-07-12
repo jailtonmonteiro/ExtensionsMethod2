1358e3d859815e255caa83a527788781c951d7ff
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-12 00:17:57 -0300

    Extensions Method na prática com strings.

---
+++ ExtensionsMethod2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionsMethod2.Extensions;
+
+namespace ExtensionsMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
